3d5b562049d9e712215978e17e397911ae9f7900
JUNGJUHYOUNG <fheh1992@gmail.com>
2021-06-09 03:12:11 +0900

    Add currentState effect

--- src/App.js
+++ src/App.js
@@ -7,13 +7,33 @@ import { GlobalStyle } from "../src/styles/GlobalStyle";
 import { Intro } from "./components/Intro/Intro";
 import { Header } from "./components/Header/Header";
 import { About } from "./components/About/About";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [page, setCurrentPage] = useState({ currentPage: null });
   const [element, isAboutInView] = useInView({
     threshold: 1,
   });
+
+  const func = () => {
+    let count = 0;
+    count++;
+    console.log(count);
+  };
+  // 단순히 렌더링 후에 사이드 이펙트를 실행하기 위해서?
+
+  func();
+  console.log(page);
+
+  useEffect(() => {
+    //switch문으로 스테이트 변경하면 될듯.
+
+    if (isAboutInView) {
+      setCurrentPage({ currentPage: "about" });
+    } else {
+      setCurrentPage({ currentPage: "no" });
+    }
+  }, [isAboutInView]);
 
   const location = useLocation();
 
